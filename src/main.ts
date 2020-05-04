df8a958cac5df079f317718435f494bb3f8a076a
dturan <dturan>
2020-05-04 16:14:14 +0200

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+/// <reference path ="../node_modules/@types/jquery/index.d.ts"/>
+/// <reference path ="../node_modules/@types/aos/index.d.ts"/>
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+declare const AOS: Aos.Aos;
+
+AOS.init({
+  duration: 800
+});
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +19,17 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+
+
+
+setTimeout(() => {
+  const header$ = $('#header');
+  function setHeader() {
+    header$.toggleClass('scrolled', window.scrollY > 0);
+  }
+  setHeader();
+  $(window).on('scroll', setHeader);
+
+}, 10);
