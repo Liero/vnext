5894e4e6e32b38a518e17cb536b103b3f032206d
Daniel Turan <daniel.turan@exxeta.sk>
2020-09-22 14:50:32 +0200

    Added contact form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { PricingComponent } from './pricing.component';
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
 import { IconComponent } from './shared/icon/icon.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent, IconComponent, PricingComponent, MenuComponent, HomeComponent,
+    AppComponent, IconComponent, PricingComponent, MenuComponent, HomeComponent, ContactComponent
   ],
   imports: [
     BrowserModule
