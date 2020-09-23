3fca28c9212d1cc81cac853603f90c327d678124
Daniel Turan <daniel.turan@exxeta.sk>
2020-09-23 15:20:26 +0200

    extracted pricing styles to component styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PricingComponent } from './pricing.component';
+import { PricingComponent } from './pricing/pricing.component';
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
 import { IconComponent } from './shared/icon/icon.component';
--- src/app/pricing/pricing.component.ts
+++ src/app/pricing/pricing.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pricing',
-  templateUrl: './pricing.component.html'
+  templateUrl: './pricing.component.html',
+  styleUrls: ['./pricing.component.scss'],
 })
 export class PricingComponent {}
