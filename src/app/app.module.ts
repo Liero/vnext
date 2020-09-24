7d12fa5c669aaa02567ade35175408503ded2941
Daniel Turan <daniel.turan@exxeta.sk>
2020-09-24 09:30:19 +0200

    extracted home and resume to separate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,17 @@ import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
 import { IconComponent } from './shared/icon/icon.component';
 import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ResumeComponent } from './resume/resume.component';
+import { SkillsComponent } from './resume/skills/skills.component';
+import { EducationComponent } from './resume/education/education.component';
+import { ExperienceComponent } from './resume/experience/experience.component';
+import { AchievementsComponent } from './resume/achievements/achievements.component';
 
 @NgModule({
   declarations: [
-    AppComponent, IconComponent, PricingComponent, MenuComponent, HomeComponent, ContactComponent
+    AppComponent, IconComponent, PricingComponent, MenuComponent, HomeComponent, ContactComponent,
+    AboutComponent, ResumeComponent, SkillsComponent, EducationComponent, ExperienceComponent, AchievementsComponent,
   ],
   imports: [
     BrowserModule
